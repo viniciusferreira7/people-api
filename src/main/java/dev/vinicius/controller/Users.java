0d8460cd45b9a5dcd006e765e1f3706dd946006e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-21 09:15:21 -0300

    feat: create users entity

--- src/main/java/dev/vinicius/controller/Users.java
+++ src/main/java/dev/vinicius/controller/Users.java
@@ -1,4 +1,20 @@
 package dev.vinicius.controller;
 
-public class Users {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class Users extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID id;
+
+    public String name;
+
+    @Column(nullable = false, unique = true)
+    public String email;
 }
