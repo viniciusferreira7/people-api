3b07cece9c933ddf9aeac5263b9be987f413324f
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-25 09:06:25 -0300

    feat: implement users service

--- src/main/java/dev/vinicius/controller/UsersController.java
+++ src/main/java/dev/vinicius/controller/UsersController.java
@@ -1,13 +1,24 @@
 package dev.vinicius.controller;
 
-import jakarta.ws.rs.GET;
+import dev.vinicius.entity.Users;
+import dev.vinicius.service.UsersService;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/users")
-public class UserController {
+public class UsersController {
 
-    @GET
-    public String getUsers(){
-        return "Hello world, Welcome to Quarkus!";
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @POST
+    public Response createUser(Users users){
+        this.usersService.create(users);
+
+        return Response.status(Response.Status.CREATED).entity(users).build();
     }
 }
--- src/main/java/dev/vinicius/service/UsersService.java
+++ src/main/java/dev/vinicius/service/UsersService.java
@@ -1,4 +1,16 @@
 package dev.vinicius.service;
 
+import dev.vinicius.entity.Users;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
 public class UsersService {
+
+    @Transactional
+    public Users create(Users users){
+        Users.persist(users);
+
+        return users;
+    }
 }
